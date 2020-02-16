39528ca3ee11c8cab6b4ff2b9830ff2b70db4cbf
anthonyg56 <anthonyg5256@gmail.com>
2020-02-16 00:41:11 -0500

    Fixed 404 payload

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,13 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/misc/seo"
 
+const payload = {
+  empty: true,
+  title: "404 error"
+}
+
 const NotFoundPage = () => (
-  <Layout>
+  <Layout payload={payload}>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
